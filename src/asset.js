3fb3747354ae89fe4e1d00e9432de1cc75039d26
HalfHydra <41239639+HalfHydra@users.noreply.github.com>
2024-04-25 23:36:35 -0400

    Improve asset.js

--- src/asset.js
+++ src/asset.js
@@ -1,8 +1,96 @@
+const achievementNameFixMap = {
+    "Clicker": "Bloons Master",
+    "Superior Clicker": "Superior Bloons Master",
+    "Ultimate Clicker": "Ultimate Bloons Master",
+    "Knowledgable Primate": "Knowledgeable Primate",
+    "Triple Threat": "Triple threat",
+    "Small Bloons": "Alchermistman and Bloonacleboy",
+    "Master of Life": "Master of life",
+    "What did it cost": "What did it cost? - Everything",
+    "2 Mega Pops": "2 MegaPops",
+    "Looking fab": "Lookin fab",
+    "GoldenTicket": "Golden Ticket",
+    "Axis of Havok": "Axis of Havoc",
+    "Co-op Popper!": "Co-op Popper",
+    "CouponCrazy": "Coupon Crazy",
+    "LimitedRun": "Limited Run",
+    "ALaCode": "A La Code",
+    "InstantGratification": "Instant Gratification",
+    "Achievement Of Achievements": "Achievement of Achievements",
+    "What is this new Bloonery": "What is this new Bloonery?",
+    "Who's the Boss": "Who's the Boss?",
+    "I'llBeBack": "I'll Be Back",
+    "SoSpiiiceyNinjaKiwi": "So Spiiicey Ninja Kiwi",
+    "DavidsVsGoliath": "Davids vs Goliath",
+    "ToolsToDarwin": "Tools to Darwin",
+    "BigSpender": "Big Spender",
+    "NotLackingCriticalInformation": "Not Lacking Critical Information",
+    "StubbornStrategy": "Stubborn Strategy",
+    "Hook Line And Sinker": "Hook, Line, and Sinker",
+    "CrashOfTheTitans": "Crash of the Titans",
+    "So Shiny": "So Shiny!",
+    "Ultimate Team Up": "Ultimate Team-up",
+    "StudentLoans": "Student Loans",
+    "StickySituation": "Sticky Situation",
+    "NoHarvest": "No Harvest",
+    "ReadyPlayerOne": "Ready Player One?",
+    "TheDailyReid": "The Daily Reid",
+    "InstaCentury": "Insta Century"
+}
+
+const skinNameFixMap = {
+    "ObynOceanGuardian": "OceanObyn",
+    "MoltenObyn": "MountainObyn",
+    "JoanOfArcAdora": "JoanOfArc",
+    "CyberQuincy": "QuincyCyber",
+    "ETnEtienne": "ETn",
+    "SentaiCaptainChurchill": "SentaiChurchill",
+    "SleighCaptainChurchill": "SleighChurchill"
+}
+
+const upgradeNameFixMap = {
+    "Spikeopult": "Spike-o-pult",
+    "BuccaneerLongRange": "LongRange",
+    "BuccaneerGrapeShot": "GrapeShot",
+    "BuccaneerCrowsNest": "CrowsNest",
+    "BuccaneerHotShot": "HotShot",
+    "BuccaneerCarrierFlagship": "CarrierFlagship",
+    "BuccaneerCannonShip": "CannonShip",
+    "BuccaneerMerchantman": "Merchantmen",
+    "BuccaneerFavoredTrades": "FavoredTrades",
+    "BuccaneerAircraftCarrier": "AircraftCarrier",
+    "BuccaneerDestroyer": "Destroyer",
+    "BuccaneerTradeEmpire": "TradeEmpire",
+    "BuccaneerPirateLord": "PirateLord",
+    "BuccaneerMonkeyPirates": "MonkeyPirates",
+    "HeattippedDarts": "HeatTippedDarts",
+    "BioncBoomerang": "BionicBoomerang",
+    "LargeCalibre": "LongCalibre",
+    "LaserBlasts": "LaserBlast",
+    "GuidedMagic": "GuildedMagic",
+    "MortarRapidReload": "RapidReload",
+    "SupplyDrop": "CashDrop",
+    "PlasmaBlasts": "PlasmaBlast",
+    "XXXLTrap": "XXXL",
+    "OversizeNails": "OversizedNails",
+    "CenteredPath": "FlightOrders",
+    "Shockwave": "ShellShock",
+    "Monkeyopolis": "Metropolis",
+    "Spectre": "Specter",
+    "TrueSunGod": "TrueSonGod",
+    "HydraRocketPods": "HydraRockets",
+    "DartMonkeyParagon": "ApexPlasmaMaster",
+    "BoomerangMonkeyParagon": "GlaiveDominus",
+    "NinjaMonkeyParagon": "AscendedShadow",
+    "EngineerMonkeyParagon": "MasterBuilder",
+    "WizardMonkeyParagon": "MagusPerfectus",
+    "MonkeyAceParagon": "GoliathDoomship",
+    "MonkeyBuccaneerParagon": "NavarchOfTheSeas",
+    "MonkeySubParagon": "NauticSiegeCore"
+}
+
 function getTowerAssetPath(towerType, upgrade) {
-    if(towerType == "WizardMonkey"){
-        towerType = "Wizard"
-    }
-    return `Assets/TowerIcon/${upgrade}-${towerType}.png`;
+    return towerType == "WizardMonkey" ? `Assets/TowerIcon/${upgrade}-Wizard.png` : `Assets/TowerIcon/${upgrade}-${towerType}.png`;
 }
 
 function getInstaMonkeyIcon(towerType, upgrade) {
@@ -10,176 +98,24 @@ function getInstaMonkeyIcon(towerType, upgrade) {
 }
 
 function getUpgradeAssetPath(upgrade) {
-    switch(upgrade) {
-        case "Spikeopult":
-            upgrade = "Spike-o-pult";
-            break;
-        case "BuccaneerLongRange":
-            upgrade = "LongRange"
-            break;
-        case "BuccaneerGrapeShot":
-            upgrade = "GrapeShot"
-            break;
-        case "BuccaneerCrowsNest":
-            upgrade = "CrowsNest"
-            break;
-        case "BuccaneerHotShot":
-            upgrade = "HotShot"
-            break;
-        case "BuccaneerCarrierFlagship":
-            upgrade = "CarrierFlagship"
-            break;
-        case "BuccaneerCannonShip":
-            upgrade = "CannonShip"
-            break;
-        case "BuccaneerMerchantman":
-            upgrade = "Merchantman"
-            break;
-        case "BuccaneerFavoredTrades":
-            upgrade = "FavoredTrades"
-            break;
-        case "BuccaneerAircraftCarrier":
-            upgrade = "AircraftCarrier"
-            break;
-        case "BuccaneerDestroyer":
-            upgrade = "Destroyer"
-            break;
-        case "BuccaneerTradeEmpire":
-            upgrade = "TradeEmpire"
-            break;
-        case "BuccaneerPirateLord":
-            upgrade = "PirateLord"
-            break;
-        case "BuccaneerMonkeyPirates":
-            upgrade = "MonkeyPirates"
-            break;
-        case "HeattippedDarts":
-            upgrade = "HeatTippedDart"
-            break;
-        case "BioncBoomerang":
-            upgrade = "BionicBoomerang"
-            break;
-        case "LargeCalibre":
-            upgrade = "LongCalibre"
-            break;
-        case "LaserBlasts":
-            upgrade = "LaserBlast"
-            break;
-        case "GuidedMagic":
-            upgrade = "GuildedMagic"
-            break;
-        case "MortarRapidReload":
-            upgrade = "RapidReload"
-            break;
-        case "SupplyDrop":
-            upgrade = "CashDrop"
-            break;
-        case "PlasmaBlasts":
-            upgrade = "PlasmaBlast"
-            break;
-        case "XXXLTrap":
-            upgrade = "XXXL"
-            break;
-        case "OversizeNails":
-            upgrade = "OversizedNails"
-            break;
-        case "CenteredPath":
-            upgrade = "FlightOrders"
-            break;
-        case "Shockwave":
-            upgrade = "ShellShock"
-            break;
-        case "Monkeyopolis":
-            upgrade = "Metropolis"
-            break;
-        case "Spectre":
-            upgrade = "Specter"
-            break;
-        case "TrueSunGod":
-            upgrade = "TrueSonGod"
-            break;
-        case "HydraRocketPods":
-            upgrade = "HydraRockets"
-            break;
-        case "DartMonkeyParagon":
-            upgrade = "ApexPlasmaMaster"
-            break;
-        case "BoomerangMonkeyParagon":
-            upgrade = "GlaiveDominus"
-            break;
-        case "NinjaMonkeyParagon":
-            upgrade = "AscendedShadow"
-            break;
-        case "EngineerMonkeyParagon":
-            upgrade = "MasterBuilder"
-            break;
-        case "WizardMonkeyParagon":
-            upgrade = "MagusPerfectus"
-            break;
-        case "MonkeyAceParagon":
-            upgrade = "GoliathDoomship"
-            break;
-        case "MonkeyBuccaneerParagon":
-            upgrade = "NavarchOfTheSeas"
-            break;
-        case "MonkeySubParagon":
-            upgrade = "NauticSiegeCore"
-            break;
-    }
-    return `Assets/UpgradeIcon/${upgrade}UpgradeIcon.png`;
+    return upgradeNameFixMap[upgrade] ? `Assets/UpgradeIcon/${upgradeNameFixMap[upgrade]}UpgradeIcon.png` : `Assets/UpgradeIcon/${upgrade}UpgradeIcon.png`;
 }
 
 function getKnowledgeAssetPath(knowledge) {
-    switch(knowledge) {
-        case "SubAdmiral":
-            knowledge = "NavalUpgrades"
-            break;
-    }
-    return `Assets/KnowledgeIcon/${knowledge}Icon.png`;
+    return knowledge == "SubAdmiral" ? `Assets/KnowledgeIcon/NavalUpgradesIcon.png` : `Assets/KnowledgeIcon/${knowledge}Icon.png`;
 }
 
 function getHeroPortrait(hero, level) {
-    if(level == 1) {
-        return `Assets/Portrait/${hero}Portrait.png`;
-    }
-    return `Assets/Portrait/${hero}PortraitLvl${level}.png`;
+    return level == 1 ?  `Assets/Portrait/${hero}Portrait.png` : `Assets/Portrait/${hero}PortraitLvl${level}.png`;
 }
 
 function getSkinAssetPath(skin, level) {
-    switch(skin){
-        case "ObynOceanGuardian":
-            skin = "OceanObyn"
-            break;
-        case "MoltenObyn":
-            skin = "MountainObyn"
-            break;
-        case "JoanOfArcAdora":
-            skin = "JoanOfArc"
-            break;
-        case "CyberQuincy":
-            skin = "QuincyCyber"
-            break;
-        case "ETnEtienne":
-            skin = "ETn"
-            break;
-        case "SentaiCaptainChurchill":
-            skin = "SentaiChurchill"
-            break;
-        case "SleighCaptainChurchill":
-            skin = "SleighChurchill"
-            break;
-    }
-    if(level == 1) {
-        return `Assets/Portrait/${skin}Portrait.png`;
-    }
-    return `Assets/Portrait/${skin}PortraitLvl${level}.png`;
+    skin = skinNameFixMap[skin] || skin;
+    return level == 1 ? `Assets/Portrait/${skin}Portrait.png` : `Assets/Portrait/${skin}PortraitLvl${level}.png`;
 }
 
 function getMapIcon(map) {
-    if(map == "#ouch") {
-        return `Assets/MapIcon/MapSelectouchButton.png`;
-    }
-    return `Assets/MapIcon/MapSelect${map}Button.png`;
+    return map == "#ouch" ? `Assets/MapIcon/MapSelectouchButton.png` : `Assets/MapIcon/MapSelect${map}Button.png`;
 }
 
 function getPowerIcon(power){
@@ -187,89 +123,9 @@ function getPowerIcon(power){
 }
 
 function getAchievementIcon(achievement, hidden){
-    if(hidden){
-        return `Assets/AchievementIcon/HiddenIcon.png`;
-    }
-    return `Assets/AchievementIcon/${achievement}Icon.png`;
+    return hidden ? `Assets/AchievementIcon/HiddenIcon.png` : `Assets/AchievementIcon/${achievement}Icon.png`;
 }
 
 function fixAchievementName(name){
-    switch(name){
-        case "Clicker":
-            return "Bloons Master"
-        case "Superior Clicker":
-            return "Superior Bloons Master"
-        case "Ultimate Clicker":
-            return "Ultimate Bloons Master"
-        case "Knowledgable Primate":
-            return "Knowledgeable Primate"
-        case "Triple Threat":
-            return "Triple threat"
-        case "Small Bloons":
-            return "Alchermistman and Bloonacleboy"
-        case "Master of Life":
-            return "Master of life"
-        case "What did it cost":
-            return "What did it cost? - Everything"
-        case "2 Mega Pops":
-            return "2 MegaPops"
-        case "Looking fab":
-            return "Lookin fab"
-        case "GoldenTicket":
-            return "Golden Ticket"
-        case "Axis of Havok":
-            return "Axis of Havoc"
-        case "Co-op Popper!":
-            return "Co-op Popper"
-        case "CouponCrazy":
-            return "Coupon Crazy"
-        case "LimitedRun":
-            return "Limited Run"
-        case "ALaCode":
-            return "A La Code"
-        case "InstantGratification":
-            return "Instant Gratification"
-        case "Achievement Of Achievements":
-            return "Achievement of Achievements"
-        case "What is this new Bloonery":
-            return "What is this new Bloonery?"
-        case "Who's the Boss":
-            return "Who's the Boss?"
-        case "I'llBeBack":
-            return "I'll Be Back"
-        case "SoSpiiiceyNinjaKiwi":
-            return "So Spiiicey Ninja Kiwi"
-        case "DavidsVsGoliath":
-            return "Davids vs Goliath"
-        case "ToolsToDarwin":
-            return "Tools to Darwin"
-        case "BigSpender":
-            return "Big Spender"
-        case "NotLackingCriticalInformation":
-            return "Not Lacking Critical Information"
-        case "StubbornStrategy":
-            return "Stubborn Strategy"
-        case "Hook Line And Sinker":
-            return "Hook, Line, and Sinker"
-        case "CrashOfTheTitans":
-            return "Crash of the Titans"
-        case "So Shiny":
-            return "So Shiny!"
-        case "Ultimate Team Up":
-            return "Ultimate Team-up"
-        case "StudentLoans":
-            return "Student Loans"
-        case "StickySituation":
-            return "Sticky Situation"
-        case "NoHarvest":
-            return "No Harvest"
-        case "ReadyPlayerOne":
-            return "Ready Player One?"
-        case "TheDailyReid":
-            return "The Daily Reid"
-        case "InstaCentury":
-            return "Insta Century"
-        default:
-            return name;
-    }
+    return achievementNameFixMap[name] || name;
 }
