80cd3dc68013df8c273990d49e8dec1da73e4716
HalfHydra <41239639+HalfHydra@users.noreply.github.com>
2025-08-05 23:19:18 -0400

    add new collection event

--- src/main.js
+++ src/main.js
@@ -9053,7 +9053,7 @@ function generateInstaSchedule() {
             padding: "10px 20px",
             textAlign: "center",
         },
-        innerHTML: "Times are your local timezone. List is not from the API, please be patient for future events"
+        innerHTML: "Times are your local timezone. List is not from the API, special thanks to Minecool" //please be patient for future events
     });
     instaScheduleHeader.appendChild(instaHeaderDescription);
 
